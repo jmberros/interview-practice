77bd1303636c484b4f02f83b82bd55eeaba99289
Juan Manuel Berros <juanmaberros@gmail.com>
2023-09-11 22:37:53 -0300

    queue via stacks

---
+++ cracking-the-coding-interview/03-04-queue-via-stacks.py
@@ -0,0 +1,47 @@
+# Queue via Stacks: Implement a MyQueue class which implements a queue using two
+# stacks.
+
+# NOTE:
+# - Queue is FIFO
+# - it can `add` (to the front) and `poll` (from the tail)
+
+class MyQueue:
+    def __init__(self):
+        self.stack1 = []
+        self.stack2 = []
+
+    def add(self, value: int):
+        while self.stack1:
+            self.stack2.append(self.stack1.pop())
+        self.stack2.append(value)  # Last value!
+        while self.stack2:
+            self.stack1.append(self.stack2.pop())
+
+    def poll(self) -> int:
+        return self.stack1.pop()
+
+    def __str__(self):
+        return f"{self.stack1 = }, {self.stack2 = }"
+
+
+q = MyQueue()
+
+q.add(1)
+print(q)
+q.add(2)
+print(q)
+q.add(3)
+print(q)
+
+assert q.poll() == 1
+print(q)
+assert q.poll() == 2
+print(q)
+
+q.add(4)
+print(q)
+
+assert q.poll() == 3
+print(q)
+assert q.poll() == 4
+print(q)
